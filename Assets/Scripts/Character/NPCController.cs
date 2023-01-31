d37dd1b81b0680ffa640aad7ee7ada6d70d376bb
schrehor <rehor.skola@gmail.com>
2023-01-31 21:19:36 +0100

    #27 dialogue system

--- Assets/Scripts/Character/NPCController.cs
+++ Assets/Scripts/Character/NPCController.cs
@@ -4,8 +4,10 @@ using UnityEngine;
 
 public class NPCController : MonoBehaviour, Interactable
 {
+    [SerializeField] Dialog dialog;
+
     public void Interact()
     {
-        Debug.Log("Interacting");
+        StartCoroutine(DialogManager.Instance.ShowDialog(dialog));
     }
 }
