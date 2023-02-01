3f1dbfb42f8f23f3dd28cb3083c0dd445f8fc549
schrehor <rehor.skola@gmail.com>
2023-02-01 19:14:11 +0100

    #28 custom animation system for characters

--- Assets/Scripts/Character/PlayerController.cs
+++ Assets/Scripts/Character/PlayerController.cs
@@ -14,12 +14,12 @@ public class PlayerController : MonoBehaviour
 
     private bool isMoving;
     private Vector2 input;
-    private Animator animator;
+    private CharacterAnimator animator;
 
 
     private void Awake()
     {
-        animator = GetComponent<Animator>();
+        animator = GetComponent<CharacterAnimator>();
     }
 
     public void HandleUpdate()
@@ -34,8 +34,8 @@ public class PlayerController : MonoBehaviour
 
             if (input != Vector2.zero)
             {
-                animator.SetFloat("moveX", input.x);
-                animator.SetFloat("moveY", input.y);
+                animator.MoveX = input.x;
+                animator.MoveY = input.y;
 
                 var targetPos = transform.position;
                 targetPos.x += input.x;
@@ -48,7 +48,7 @@ public class PlayerController : MonoBehaviour
             }
         }
 
-        animator.SetBool("isMoving", isMoving);
+        animator.IsMoving = isMoving;
 
         if (Input.GetKeyDown(KeyCode.X))
         {
@@ -58,7 +58,7 @@ public class PlayerController : MonoBehaviour
 
     void Interact()
     {
-        var facingDirection = new Vector3(animator.GetFloat("moveX"), animator.GetFloat("moveY"));
+        var facingDirection = new Vector3(animator.MoveX, animator.MoveY);
         var interactPos = transform.position + facingDirection;
 
         var collider = Physics2D.OverlapCircle(interactPos, 0.3f, interactableLayer);
@@ -90,7 +90,7 @@ public class PlayerController : MonoBehaviour
         {
             if (UnityEngine.Random.Range(1, 101) <= 10)
             {
-                animator.SetBool("isMoving", false);
+                animator.IsMoving = false;
                 OnEncountered();
             }
         }
