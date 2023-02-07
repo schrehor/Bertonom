4ac61727ec2b4c6305a037dd6eaa10bfe2d975de
schrehor <rehor.skola@gmail.com>
2023-02-07 22:18:14 +0100

    documentation comments for Characters.cs

--- Assets/Scripts/Character/Character.cs
+++ Assets/Scripts/Character/Character.cs
@@ -10,11 +10,18 @@ public class Character : MonoBehaviour
 
     public bool IsMoving { get; private set; }
 
+    public CharacterAnimator Animator { get => animator; }
+
     private void Awake()
     {
         animator = GetComponent<CharacterAnimator>();
     }
 
+    /// <summary>
+    /// If the path to the moveVec is accesible, the coroutine moves the target to the given location.
+    /// </summary>
+    /// <param name="moveVec">Destination vector</param>
+    /// <param name="OnMoveOver">Action that invokes methods after the target finished moving</param>
     public IEnumerator Move(Vector2 moveVec, Action OnMoveOver = null)
     {
         animator.MoveX = Mathf.Clamp(moveVec.x, -1f, 1f);
@@ -38,15 +45,23 @@ public class Character : MonoBehaviour
         }
         //transform.position = targetPos;
         IsMoving = false;
-
+        
         OnMoveOver?.Invoke();
     }
 
+    /// <summary>
+    /// Updates the IsMoving parameter of the animator
+    /// </summary>
     public void HandleUpdate()
     {
         animator.IsMoving = IsMoving;
     }
 
+    /// <summary>
+    /// Checks if the path from the caller to the given targetPos is clear.
+    /// </summary>
+    /// <param name="targetPos">The target position of the caller.</param>
+    /// <returns>True if the path is clear</returns>
     private bool IsPathClear(Vector3 targetPos)
     {
         var diff = targetPos - transform.position;
@@ -71,11 +86,15 @@ public class Character : MonoBehaviour
         return true;
     }
 
+    /// <summary>
+    /// Makes the character face the targetPos.
+    /// </summary>
+    /// <param name="targetPos">Character which we want to face.</param>
     public void LookTowards(Vector3 targetPos)
     {
         var xDiff = Mathf.Floor(targetPos.x) - Mathf.Floor(transform.position.x);
         var yDiff = Mathf.Floor(targetPos.y) - Mathf.Floor(transform.position.y);
-
+       
         if (xDiff == 0 || yDiff == 0)
         {
             animator.MoveX = Mathf.Clamp(xDiff, -1f, 1f);
@@ -86,6 +105,4 @@ public class Character : MonoBehaviour
             Debug.LogError("Error in Look Towards: You can't ask the character to look diagonal");
         }
     }
-
-    public CharacterAnimator Animator { get => animator; }
 }
