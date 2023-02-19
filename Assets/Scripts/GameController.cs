8370a5d36df161adf6c31246ed8a2314879fabac
schrehor <rehor.skola@gmail.com>
2023-02-19 16:41:40 +0100

    #44 additive scene loading

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -13,6 +13,9 @@ public class GameController : MonoBehaviour
     GameState prevState;
     TrainerController trainer;
 
+    public SceneDetails CurrentScene { get; private set; }
+    public SceneDetails PrevScene { get; private set; }
+
     public static GameController Instance { get; private set; }
 
     private void Awake()
@@ -112,5 +115,11 @@ public class GameController : MonoBehaviour
         {
             DialogManager.Instance.HandleUpdate();
         }
+    }
+
+    public void SetCurrentScene(SceneDetails currScene)
+    {
+        PrevScene = CurrentScene;
+        CurrentScene = currScene;
     }
 }
