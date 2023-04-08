827d19926e7d5f3de29f32a48224ecc9d77fbb3f
schrehor <rehor.skola@gmail.com>
2023-04-08 13:27:41 +0200

    saving if pickup is taken

--- Assets/Scripts/Inventory/Pickup.cs
+++ Assets/Scripts/Inventory/Pickup.cs
@@ -1,7 +1,8 @@
 using System.Collections;
 using UnityEngine;
+using UnityEngine.UIElements;
 
-public class Pickup : MonoBehaviour, Interactable
+public class Pickup : MonoBehaviour, Interactable, ISavable
 {
     [SerializeField] private ItemBase item;
     public bool Used { get; set; }
@@ -19,6 +20,22 @@ public class Pickup : MonoBehaviour, Interactable
             string playerName = initiator.GetComponent<PlayerController>().Name;
             
             yield return DialogManager.Instance.ShowDialogText($"{playerName} found {item.Name}");
+        }
+    }
+
+    public object CaptureState()
+    {
+        return Used;
+    }
+
+    public void RestoreState(object state)
+    {
+        Used = (bool) state;
+
+        if (Used)
+        {
+            GetComponent<SpriteRenderer>().enabled = false;
+            GetComponent<BoxCollider2D>().enabled = false;
         }
     }
 }
