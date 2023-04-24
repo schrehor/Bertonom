e41879b13d56f97308a80c13b88d8b679e68a93b
schrehor <rehor.skola@gmail.com>
2023-04-24 14:20:02 +0200

    enable/disable objects based on quest progression

--- Assets/Scripts/Quests/QuestList.cs
+++ Assets/Scripts/Quests/QuestList.cs
@@ -1,17 +1,34 @@
-using System.Collections;
+using System;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 public class QuestList : MonoBehaviour
 {
     List<Quest> quests = new List<Quest>();
 
+    public event Action OnUpdated;
+    
     public void AddQuest(Quest quest)
     {
         if (!quests.Contains(quest))
         {
             quests.Add(quest);
         }
+        
+        OnUpdated?.Invoke();
+    }
+
+    public bool IsStarted(string questName)
+    {
+        var questStatus = quests.FirstOrDefault(q => q.Base.Name == questName)?.Status;
+        return questStatus is QuestStatus.Started or QuestStatus.Completed;
+    }
+    
+    public bool IsCompleted(string questName)
+    {
+        var questStatus = quests.FirstOrDefault(q => q.Base.Name == questName)?.Status;
+        return questStatus is QuestStatus.Completed;
     }
 
     public static QuestList GetQuestList()
---
+++ Assets/Scripts/Quests/QuestObject.cs
@@ -0,0 +1,61 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class QuestObject : MonoBehaviour
+{
+    [SerializeField] private QuestBase questToCheck;
+    [SerializeField] private ObjectActions onStart;
+    [SerializeField] private ObjectActions onComplete;
+
+    private QuestList _questList;
+
+    private void Start()
+    {
+        _questList = QuestList.GetQuestList();
+        _questList.OnUpdated += UpdateObjectStatus;
+        
+        UpdateObjectStatus();
+    }
+
+    private void OnDestroy()
+    {
+        _questList.OnUpdated -= UpdateObjectStatus;
+    }
+
+    public void UpdateObjectStatus()
+    {
+        if (onStart != ObjectActions.DoNothing && _questList.IsStarted(questToCheck.Name))
+        {
+            foreach (Transform child in transform)
+            {
+                if (onStart == ObjectActions.Enable)
+                {
+                    child.gameObject.SetActive(true);
+                }
+                else if (onStart == ObjectActions.Disable)
+                {
+                    child.gameObject.SetActive(false);
+                }
+            }
+        }
+
+        if (onComplete != ObjectActions.DoNothing && _questList.IsCompleted(questToCheck.Name))
+        {
+            foreach (Transform child in transform)
+            {
+                if (onComplete == ObjectActions.Enable)
+                {
+                    child.gameObject.SetActive(true);
+                }
+                else if (onComplete == ObjectActions.Disable)
+                {
+                    child.gameObject.SetActive(false);
+                }
+            }
+        }
+    }
+}
+
+public enum ObjectActions { DoNothing, Enable, Disable }
