6e7a1d5d6939b1b1e540a4b63565964ec6c08268
schrehor <rehor.skola@gmail.com>
2023-05-10 16:59:31 +0200

    implementing fading when healing pokemon

--- Assets/Scripts/Character/Healer.cs
+++ Assets/Scripts/Character/Healer.cs
@@ -7,8 +7,11 @@ public class Healer : MonoBehaviour
     public IEnumerator Heal(Transform player, Dialog dialog)
     {
         yield return DialogManager.Instance.ShowDialog(dialog);
+
+        yield return Fader.Instance.FadeIn(0.5f);
         var playerParty = player.GetComponent<PokemonParty>();
         playerParty.Pokemons.ForEach(x => x.Heal());
         playerParty.PartyUpdated();
+        yield return Fader.Instance.FadeOut(0.5f);
     }
 }
