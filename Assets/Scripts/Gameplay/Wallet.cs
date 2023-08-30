889d9fa1839e46e142413ba0bcd6149a8bc9afd2
schrehor <rehor.skola@gmail.com>
2023-08-30 19:23:44 +0200

    #84 Saving money

--- Assets/Scripts/Gameplay/Wallet.cs
+++ Assets/Scripts/Gameplay/Wallet.cs
@@ -3,7 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Wallet : MonoBehaviour
+public class Wallet : MonoBehaviour, ISavable
 {
     [SerializeField] private float money;
     
@@ -32,5 +32,15 @@ public class Wallet : MonoBehaviour
     public bool HasEnoughMoney(float amount)
     {
         return money >= amount;
+    }
+
+    public object CaptureState()
+    {
+        return money;
+    }
+
+    public void RestoreState(object state)
+    {
+        money = (float) state;
     }
 }
