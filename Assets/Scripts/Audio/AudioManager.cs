42b679eb24edcba6dea794a15a64a75dd0ba993a
schrehor <rehor.skola@gmail.com>
2023-10-03 22:59:00 +0200

    System to play sound effects by an Enum Id

--- Assets/Scripts/Audio/AudioManager.cs
+++ Assets/Scripts/Audio/AudioManager.cs
@@ -1,16 +1,20 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using DG.Tweening;
 using UnityEngine;
 
 public class AudioManager : MonoBehaviour
 {
+    [SerializeField] private List<AudioData> sfxList;
+    
     [SerializeField] private AudioSource musicPlayer;
     [SerializeField] private AudioSource sfxPlayer;
     [SerializeField] private float fadeDuration = 0.75f;
 
     private float _originalMusicVolume;
+    private Dictionary<AudioId, AudioData> _sfxLookup;
     public static AudioManager Instance { get; private set; }
     
     private void Awake()
@@ -21,8 +25,32 @@ public class AudioManager : MonoBehaviour
     private void Start()
     {
         _originalMusicVolume = musicPlayer.volume;
+
+        _sfxLookup = sfxList.ToDictionary(x => x.id);
     }
 
+    public void PlaySfx(AudioClip clip)
+    {
+        if (clip == null)
+        {
+            return;
+        }
+        
+        sfxPlayer.PlayOneShot(clip);
+    }
+    
+    public void PlaySfx(AudioId audioId)
+    {
+        if (!_sfxLookup.ContainsKey(audioId))
+        {
+            return;
+        }
+        
+        var audioData = _sfxLookup[audioId];
+        PlaySfx(audioData.clip);
+    }
+    
+            
     public void PlayMusic(AudioClip clip, bool loop = true, bool fade = false)
     {
         if (clip == null)
@@ -49,4 +77,13 @@ public class AudioManager : MonoBehaviour
             yield return musicPlayer.DOFade(_originalMusicVolume, fadeDuration).WaitForCompletion();
         }
     }
+}
+
+public enum AudioId { UISelect, Hit, Faint, ExpGain }
+
+[Serializable]
+public class AudioData
+{
+    public AudioId id;
+    public AudioClip clip;
 }
